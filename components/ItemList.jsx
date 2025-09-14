0a4ce13a355c4fca25acaf87c96f91b6ad2ab9ca
Shiva Pandit <shivakrwpa1@gmail.com>
2025-09-15 00:58:39 +0530

    fix: resolve accordion toggle/display issue

--- components/ItemList.jsx
+++ components/ItemList.jsx
@@ -1,13 +1,37 @@
 const ItemList = ({ items }) => {
-    console.log(items);
+  console.log("items", items);
+  const IMG_CDN_URL =
+    "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_400/";
 
   return (
     <div>
       {items.map((item) => (
-        <div key={item.card.info.id} className="m-2 p-2 border-gray-200 border-b-1">
-          <span>{item.card.info.name}</span>
-          <span>{item.card.info.description}</span>
-        </div>  
+        <div
+          key={item.card.info.id}
+          className="m-2 p-4 border-b border-gray-200 flex justify-between items-start"
+        >
+          <div className="w-8/12">
+            <h3 className="font-semibold text-lg">
+              {item.card.info.name} - ₹
+              {item.card.info.defaultPrice
+                ? item.card.info.defaultPrice / 100
+                : item.card.info.price / 100}
+            </h3>
+            <p className="text-sm text-gray-600 mt-2">
+              {item.card.info.description}
+            </p>
+          </div>
+          <div className="relative w-32 h-32">
+            <img
+              src={IMG_CDN_URL + item.card.info.imageId}
+              alt={item.card.info.name}
+              className="w-full h-full object-cover rounded-lg"
+            />
+            <button className="absolute bottom-2 left-1/2 -translate-x-1/2 bg-white rounded-md px-2 py-1 text-sm shadow-md">
+              Add +
+            </button>
+          </div>
+        </div>
       ))}
     </div>
   );
--- components/RestaurantCategory.jsx
+++ components/RestaurantCategory.jsx
@@ -1,19 +1,24 @@
+import { useState } from "react";
 import ItemList from "./ItemList";
 
 const RestaurantCategory = ({ data }) => {
-  
+  const [showItem, setShowItem] = useState(false);
+  const handleClick = () => {
+    setShowItem(!showItem)
+    // console.log("clicked");
+  };
 
   return (
     <div>
       <div className="w-6/12 bg-gray-50 shadow-lg p-4 mx-auto my-6 ">
-        <div className="flex justify-between">
+        <div className="flex justify-between" onClick={handleClick}>
           <span className="font-bold text-lg">
             {data.title} ({data.itemCards.length})
           </span>
           <span>⬇️</span>
         </div>
 
-        <ItemList items={data.itemCards} />
+        {showItem && <ItemList items={data.itemCards} />}
       </div>
     </div>
   );
